f9a994ffae5e3628124f146f47a05f3e4ed6c2fa
nyambura00 <winnienyambura14@gmail.com>
2022-04-23 17:04:16 -0400

    establish frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,44 @@
-import './App.css';
-import React, { useCallback, useEffect, useState } from "react";
-import { getEvents } from "./utils/meetups";
-import { login } from "./utils/near";
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-function App() {
-  const account = window.walletConnection.account();
-  const [events, viewEvents] = useState([]);
-  const fetchEvents = useCallback(async () => { //caching management
-    if (account.accountId) {
-      viewEvents(await getEvents());
-    }
-  }, [account.accountId]);
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
   useEffect(() => {
-    fetchEvents();
-  });
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <>
-      {account.accountId ? (
-        events.forEach((event) => console.log(event))
-      ) : (
-        <button onClick={login}>CONNECT WALLET</button>
-      )}
-    </>
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
-import { initializeContract } from './utils/near';
+//import './App.css';
+import './assets/scss/style.scss';
 
-window.nearInitPromise = initializeContract()
-  .then(() => {
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  })
-  .catch(console.error);
+const history = createBrowserHistory();
 
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
