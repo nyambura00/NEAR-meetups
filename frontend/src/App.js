d227f77df809d759a708c822688192f2edd6295b
nyambura00 <winnienyambura14@gmail.com>
2022-04-24 12:30:42 -0400

    set frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,27 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch } from 'react-router-dom';
-import AppRoute from './utils/AppRoute';
-import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
+import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import { getEvents } from "./utils/meetups";
+import { login } from "./utils/near";
 
-// Layouts
-import LayoutDefault from './layouts/LayoutDefault';
-
-// Views 
-import Home from './views/Home';
-
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
-
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
-
-const App = () => {
-
-  const childRef = useRef();
-  let location = useLocation();
-
+function App() {
+  const account = window.walletConnection.account();
+  const [events, viewEvents] = useState([]);
+  const fetchEvents = useCallback(async () => { //caching management
+    if (account.accountId) {
+      viewEvents(await getEvents());
+    }
+  }, [account.accountId]);
   useEffect(() => {
-    const page = location.pathname;
-    document.body.classList.add('is-loaded')
-    childRef.current.init();
-    trackPage(page);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
-
+    fetchEvents();
+  });
   return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-        <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-        </Switch>
-      )} />
+    <>
+      {account.accountId ? (
+        events.forEach((event) => console.log(event))
+      ) : (
+        <button onClick={login}>CONNECT WALLET</button>
+      )}
+    </>
   );
 }
 
