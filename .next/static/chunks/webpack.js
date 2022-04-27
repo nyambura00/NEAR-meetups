13054ec2b4013566b63b8356bd89f451594acf90
nyambura00 <winnienyambura14@gmail.com>
2022-04-27 17:07:58 -0400

    window object ref error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4d0a106c45ee20f55897";
+/******/ 	var hotCurrentHash = "51179c89c76eac53b82f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,16 @@ import { AppProps } from 'next/app';
 import '../styles/global.css';
 import '@fontsource/inter';
 
-import { setup } from 'twind';
+// import { setup } from 'twind';
 import { AppProvider } from '../components/AppContext';
-import twindConfig from '../twind.config';
+// import twindConfig from '../twind.config';
 
-if (typeof window !== `undefined`) {
-  setup(twindConfig);
-}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const AnyComponent = Component as any;
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <AnyComponent {...pageProps} />
     </AppProvider>
   );
 }
