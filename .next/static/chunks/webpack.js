00004eb9a6941a2bafbbf5b5ec2b3d99b12a3350
nyambura00 <winnienyambura14@gmail.com>
2022-04-29 02:09:34 -0400

    Update textarea element

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0c82d566ddeda0d43852";
+/******/ 	var hotCurrentHash = "b4537b5417e03497821b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/addevent.tsx
+++ pages/addevent.tsx
@@ -17,7 +17,7 @@ const AddEvent = () => {
 
                         <div>
                             <label className="text-gray-700 dark:text-gray-200">Event Description</label>
-                            <input id="event-description" type="textarea" placeholder="event description" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                            <textarea id="event-description" placeholder="event description" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                         </div>
 
                         <div>
