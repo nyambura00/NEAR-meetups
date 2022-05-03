336c354fd85b5f77a6c0c5f9c791af38198a1182
nyambura00 <winnienyambura14@gmail.com>
2022-05-03 11:58:35 -0400

    Module not found: Can't resolve 'near-sdk-as'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "85ee23df18c1ab1d"; }
+/******/ 		__webpack_require__.h = function() { return "f5b943b593e73674"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
